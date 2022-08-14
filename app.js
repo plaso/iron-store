07a51a3a19324333607c22d6fa35b45d231398a2
marco238 <marco238cuba@gmail.com>
2022-08-14 15:43:18 +0200

    update router name

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const router = require("./config/router.config");
+const router = require("./config/index.routes");
 app.use(router);
 
 app.use((err, req, res, next) => {
