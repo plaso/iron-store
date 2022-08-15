02e45de667333fe82a67f106e77a9f4794dfac53
marco238 <marco238cuba@gmail.com>
2022-08-15 15:11:40 +0200

    session comments

--- config/session.config.js
+++ config/session.config.js
@@ -2,7 +2,7 @@ const expressSession = require("express-session");
 const MongoStore = require("connect-mongo");
 const { DB } = require("./db.config");
 
-const sessionMaxAge = process.env.SESSION_AGE || 7;
+const sessionMaxAge = process.env.SESSION_AGE || 7; // days
 
 const sessionConfig = expressSession({
   secret: process.env.COOKIE_SECRET || "Super secret (change it!)",
@@ -10,12 +10,12 @@ const sessionConfig = expressSession({
   saveUninitialized: false,
   cookie: {
     secure: process.env.COOKIE_SECURE === "true" || false,
-    maxAge: 24 * 3600 * 1000 * sessionMaxAge,
+    maxAge: 24 * 60 * 60 * 1000 * sessionMaxAge, // 1 week
     httpOnly: true,
   },
   store: new MongoStore({
     mongoUrl: DB,
-    ttl: 24 * 3600 * sessionMaxAge,
+    ttl: 24 * 60 * 60 * 1000 * sessionMaxAge, // 1 week
   }),
 });
 
