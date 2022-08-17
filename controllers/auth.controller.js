f9fdbade5c3bb9b2a9fda4b70796595eacb18466
Marcos Monzón <marcomonzip@gmail.com>
2022-08-17 15:17:27 +0200

    refactor doLogin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -58,23 +58,19 @@ module.exports.login = (req, res, next) => {
 module.exports.doLogin = (req, res, next) => {
   login(req, res, next)
 
-  // const renderWithErrors = (errors) => {
-  //   res.render("auth/login", { errors });
-  // };
-
   // const { email, password } = req.body;
 
   // User.findOne({ email })
   //   .then((user) => {
   //     if (!user) {
-  //       renderWithErrors("Invalid credentials.");
+  //       res.render("auth/login", { errors: "Invalid credentials." });
   //     } else if (user) {
   //       user.checkPassword(password).then((match) => {
   //         if (match) {
   //           req.session.currentUser = user;
   //           res.redirect("/profile");
   //         } else {
-  //           renderWithErrors("Invalid credentials.");
+  //           res.render("auth/login", { errors: "Invalid credentials." });
   //         }
   //       });
   //     }
