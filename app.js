b94b23a44fe133186df9643fc866de8e6874d657
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2022-09-10 14:19:47 +0200

    update port

--- app.js
+++ app.js
@@ -32,4 +32,4 @@ app.use((err, req, res, next) => {
   res.render("error", { err });
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Listening on port 3000"));
