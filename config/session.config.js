6c33ce4469f5e26f09d21b64fcd6749cf0cac7d5
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2022-09-10 14:47:30 +0200

    domain

--- config/session.config.js
+++ config/session.config.js
@@ -12,7 +12,7 @@ const sessionConfig = expressSession({
     secure: process.env.COOKIE_SECURE === "true" || false,
     maxAge: 24 * 60 * 60 * 1000 * sessionMaxAge, // 1 week
     httpOnly: true,
-    domain: '.railway.app'
+    ...(process.env.COOKIE_DOMAIN ? { domain: process.env.COOKIE_DOMAIN } : {}),
   },
   store: new MongoStore({
     mongoUrl: DB,
